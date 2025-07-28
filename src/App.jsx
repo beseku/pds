a9219d4396c68e5192bc10230a3e14bc84092193
Ben Sekulowicz-Barclay <ben@seku.co>
2025-07-28 12:25:27 +0200

    Fix links

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
               <h1 style={{ color: "var(--color-foreground-neutral-weak)", fontFamily: "var(--font-family-text)", fontFeatureSettings: "var(--font-feature-settings-heading)", fontSize: "var(--font-size-2xs)", fontWeight: "var(--font-weight-heading)", letterSpacing: "0.1em", lineHeight: "var(--line-height-2xs)", textTransform: "uppercase" }}>Inputs</h1>
               <ul style={{ display: "flex", flexDirection: "column", alignItems: "stretch", justifyContent: "flexStart", gap: "var(--magin-tighter)" }}>
                 <li style={{  }}>
-                  <a href="/text-input" style={{ color: "var(--color-foreground-neutral)", fontFamily: "var(--font-family-text)", fontFeatureSettings: "var(--font-feature-settings-text)", fontSize: "var(--font-size-sm)", fontWeight: "var(--font-weight-text)", lineHeight: "var(--line-height-sm)", textDecoration: "none" }}>Text Input</a>
+                  <Link style={{ color: "var(--color-foreground-neutral)", fontFamily: "var(--font-family-text)", fontFeatureSettings: "var(--font-feature-settings-text)", fontSize: "var(--font-size-sm)", fontWeight: "var(--font-weight-text)", lineHeight: "var(--line-height-sm)", textDecoration: "none" }} to="/text-input">Text Input</Link>
                 </li>
               </ul>
             </div>
